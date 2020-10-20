29e7070a383f52c4d4a8591e61c5993bc076cbb3
Sergey Belyshkin <4sb@mail.ru>
2020-10-20 19:53:51 +0700

    OOP-1: Neo knows Kung Fu

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ require_once './vendor/autoload.php';
 $customer = new Customer(1, 'John', '100.55', '34');
 $seller = new Seller(2, 'Alex', '550.87', '781');
 $administrator = new Administrator(3, 'Arnold', '{reports, sales, users}');
+$neo = new Administrator(0, 'Neo', '{kung-fu}');
 
 $userManager = new UserManager();
 
@@ -17,4 +18,5 @@ echo $userManager->getUserInfo($customer);
 echo $userManager->getUserInfo($seller);
 echo $userManager->getUserInfo($administrator);
 
-echo "Hello world!" . PHP_EOL;
+echo PHP_EOL . "Welcome to the real world!";
+echo $userManager->getUserInfo($neo);
