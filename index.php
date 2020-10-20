585718a07975ec2201761981a71e49d9e5b605ad
Sergey Belyshkin <4sb@mail.ru>
2020-10-20 19:49:40 +0700

    OOP-1: embedding new feature

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
 
+use user\Administrator;
+use user\Customer;
+use user\Seller;
+use \user\UserManager;
+
+require_once './vendor/autoload.php';
+
+$customer = new Customer(1, 'John', '100.55', '34');
+$seller = new Seller(2, 'Alex', '550.87', '781');
+$administrator = new Administrator(3, 'Arnold', '{reports, sales, users}');
+
+$userManager = new UserManager();
+
+echo $userManager->getUserInfo($customer);
+echo $userManager->getUserInfo($seller);
+echo $userManager->getUserInfo($administrator);
+
 echo "Hello world!" . PHP_EOL;
